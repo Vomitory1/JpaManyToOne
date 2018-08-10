f379f310c7af67317d003cc3f9f4550aa0eaa148
Vomitory1 <jhoncstr290@gmail.com>
2018-08-09 21:46:59 -0500

    rtrt

---
+++ src/main/java/com/example/JpaManyToOne/JpaManyToOneApplication.java
@@ -0,0 +1,12 @@
+package com.example.JpaManyToOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaManyToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaManyToOneApplication.class, args);
+	}
+}
